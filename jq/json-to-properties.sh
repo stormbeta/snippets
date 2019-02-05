8b594b1dd05e6697d2a6690ce81c2e10cb52bd78
Jason Miller <stormbeta@gmail.com>
2019-02-05 10:56:28 -0700

    Fix to allow use with go-based yq

--- jq/json-to-properties.sh
+++ jq/json-to-properties.sh
@@ -8,7 +8,12 @@ jq_cmd='. as $orig |
     (. | join(".")) + "=" + (. as $p | idx($p[]) | tostring)'
 
 if command -v yq &>/dev/null; then
-  yq . "$1" | jq "$jq_cmd" -r
+  # Go-based yq is completely different from the python version
+  if yq --version 2>&1 | grep -q 'yq version' >/dev/null; then
+    yq r -j "$1" | jq "$jq_cmd" -r
+  else
+    yq . "$1" | jq "$jq_cmd" -r
+  fi
 else
   jq "$jq_cmd" -r "$1"
 fi
