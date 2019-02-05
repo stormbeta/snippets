65ccbb0a314dc328f8e510bf548679ad7918e0a2
Jason Miller <stormbeta@gmail.com>
2019-02-05 10:47:11 -0700

    Make generic so that it works with json / without yq
    The idea was to be minimal dependency

--- jq/json-to-properties.sh
+++ jq/json-to-properties.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-yq . "$1" | \
-  jq '. as $orig |
+jq_cmd='. as $orig |
     def idx(pth):
       $orig | recurse(.[pth]? // empty) |
       select(type | . != "object" and . != "array");
     $orig | path(.. | select(type | . != "object" and . != "array")) |
-    (. | join(".")) + "=" + (. as $p | idx($p[]) | tostring)' -r
+    (. | join(".")) + "=" + (. as $p | idx($p[]) | tostring)'
+
+if command -v yq &>/dev/null; then
+  yq . "$1" | jq "$jq_cmd" -r
+else
+  jq "$jq_cmd" -r "$1"
+fi
